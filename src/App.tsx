3cd68e1ec4101e2b69b08ee1e3070f4b3ab0e855
cod1r <jasonho353@gmail.com>
2024-04-05 19:26:40 -0500

    bubbles and prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,28 @@
-import { useState } from 'react'
-import './App.css'
+import { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
+  const [bubbles, setBubbles] = useState([]);
+  useEffect(() => {}, []);
   return (
-    <div className="container">
+    <div onClick={(e) => console.log(e.target.key)} className="container">
+      {bubbles.map((b, idx) => (
+        <div
+          key={`${idx}-${b}`}
+          onClick={() => {}}
+          style={{
+            width: "60px",
+            height: "60px",
+            backgroundImage:
+              "radial-gradient(circle at center, transparent 0, lightblue 90%, blue 100%)",
+            position: "absolute",
+            top: `${b}px`,
+            borderRadius: "100%",
+          }}
+        ></div>
+      ))}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
